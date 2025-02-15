0e58dbdc44bc49f7de5b280e636819ef48f6fed2
ttmariarti <dupinm21@gmail.com>
2025-02-15 11:34:33 +0200

    upd
    asds

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const name = prompt("Enter your name");
 console.log(name);
 const lastname = prompt("Your Lastname");
 console.log(lastname)
-let = prompt("Your Age");
-let = prompt("Your email");
+let age = prompt("Your Age");
+let email = prompt("Your email");
